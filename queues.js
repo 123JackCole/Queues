f948f9646d269b4a782b3e056841d3bcf425ecf1
123JackCole <123jack.cole@gmail.com>
2020-07-18 18:27:12 -0700

    addded test data

--- queues.js
+++ queues.js
@@ -9,3 +9,14 @@ Queue.prototype.enqueue = function(data) {
 Queue.prototype.dequeue = function() {
     return this.elements.shift();
 }
+
+// create a queue
+testQueue = new Queue();
+
+// add some elements to the queue
+testQueue.prototype.enqueue(9);
+testQueue.prototype.enqueue(2);
+
+// remove the first element from the queue
+// in this case 9
+testQueue.prototype.dequeue();
