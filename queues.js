299f03df8b949ec416abf1ac456fd0db62b618c3
123JackCole <123jack.cole@gmail.com>
2020-07-18 18:26:36 -0700

    added enqueue and dequeue

--- queues.js
+++ queues.js
@@ -1,3 +1,11 @@
 function Queue() {
     this.elements = [];
 }
+
+Queue.prototype.enqueue = function(data) {
+    this.elements.push(data);
+}
+
+Queue.prototype.dequeue = function() {
+    return this.elements.shift();
+}
